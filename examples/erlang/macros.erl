a1a16cd85fe5a7aaa4ae7c9c44d939ecbe6a8879
travis wade <wade.travis@gmail.com>
2015-07-24 23:33:48 -0700

    example macros

---
+++ examples/erlang/macros.erl
@@ -0,0 +1,12 @@
+-module(macros).
+-define(a, 1).
+-define(wrap(A), {ok, A}).
+-define(appl1(A, B), A(B)).
+-define(appl2(A, B), B(A)).
+
+use1 () -> ?a.
+
+wrap () -> ?wrap(1).
+
+appl1 () -> ?appl1(fun(X)->X+1 end, 1 ).
+appl2 () -> ?appl2(1, fun(X)->X+1 end). 
